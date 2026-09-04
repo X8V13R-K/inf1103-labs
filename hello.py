b9769933adfb0ba8a66fe634cfe2883798aecf52
Xavier Kua <2603035@sit.singaporetech.edu.sg>
2026-09-04 15:05:38 +0800

    Fourth Post

--- hello.py
+++ hello.py
@@ -22,3 +22,15 @@ print ("Day 2: ", followers)
 
 followers -= 10
 print ("Day 3: ", followers)
+
+print ("=======================")
+username = input("Enter your username: ")
+age = input("Enter your age: ")
+category = input("Enter your content category: ")
+
+print ("\nInstagram Profile")
+print ("========================")
+print ("Username: ", username)
+print ("Age: ", age)
+print ("Category: ", category)
+
