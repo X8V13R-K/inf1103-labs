730a55f89263641166c1d6c9dde803868b9b574c
Xavier Kua <2603035@sit.singaporetech.edu.sg>
2026-09-11 18:46:28 +0800

    Lab 2.2

--- Week 2/auditor.py
+++ Week 2/auditor.py
@@ -3,3 +3,14 @@ unitsProcessed = 0
 numFailedEntrys = 0
 while True:
     qty = input("Stock Quantity:")
+
+    if qty.isdigit(): 
+        #Valid input
+        inventory += int(qty)
+        unitsProcessed += 1
+        #print("Inventory:", inventory)
+
+        if inventory > 500: 
+            #Inventory count exceeds 500
+            print("Warning: Inventory exceeds 500 units.")
+            break
